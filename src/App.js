8af55521958590bce66ae0829b67ef8c7b0309e7
marinasatsyk <76702131+marinasatsyk@users.noreply.github.com>
2022-09-01 12:16:05 +0200

    deleting  unsed code

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
-// import logo from './logo.svg';
 import {
     BrowserRouter as Router,
     Route,
     Routes,
-    useLocation,
     Navigate,
 } from 'react-router-dom';
 import './App.css';
 import Header from './components/header';
-import Main from './components/body';
 import Footer from './components/footer';
 import Langing from './components/body';
 import CV from './components/body/cv';
 import Skills from './components/body/skills';
 import Error from './components/error';
 import Projects from './components/body/projects';
-import videoBg from './assets/videoBg.mp4';
-import { useEffect } from 'react';
-import particlesConfig from './assets/particlesConfig.json';
 import ParticlesBg from './components/particles';
-// import videoBg from './assets/Map.mp4';
 
 function App() {
     document.title = 'DARDE Marina';
-    // useEffect(() => {
-    //     const script = document.createElement('script');
-    //     script.src = './components/bg_animation/work_part/particles.js';
-    //     script.async = true;
-    //     return () => {
-    //         document.body.removeChild(script);
-    //     };
-    // }, []);
 
     return (
         <Router>
             <div id="wrapper_body">
                 <Header />
                 <main>
-                    {/* <div id="particles-js"></div>
-                    <div class="count-particles">
-                        <span class="js-count-particles">--</span> particles
-                    </div> */}
                     <ParticlesBg />
                     <div id="main-content">
                         <Routes>
@@ -62,10 +43,8 @@ function App() {
                             />
                         </Routes>
                     </div>
-                    {/* <video src={videoBg} autoPlay muted loop /> */}
                 </main>
 
-                {/* <Main /> */}
                 <Footer />
             </div>
         </Router>
--- src/components/particles/index.jsx
+++ src/components/particles/index.jsx
@@ -1,91 +1,11 @@
-import { useMemo, useCallback } from 'react';
+import { useCallback } from 'react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import options from '../../assets/particlesConfig.json';
 
 const ParticlesBg = () => {
-    // const options = useMemo(() => {
-    //     return {
-    //         background: {
-    //             color: {
-    //                 value: '#023968',
-    //             },
-    //         },
-    //         fullScreen: {
-    //             zIndex: 0,
-    //         },
-    //         fpsLimit: 120,
-    //         // interactivity: {
-    //         //     events: {
-    //         //         onClick: {
-    //         //             enable: true,
-    //         //             mode: 'push',
-    //         //         },
-    //         //         onHover: {
-    //         //             enable: true,
-    //         //             mode: 'repulse',
-    //         //         },
-    //         //         resize: true,
-    //         //     },
-    //         //     modes: {
-    //         //         push: {
-    //         //             quantity: 4,
-    //         //         },
-    //         //         repulse: {
-    //         //             distance: 150,
-    //         //             duration: 0.4,
-    //         //         },
-    //         //     },
-    //         // },
-    //         particles: {
-    //             color: {
-    //                 value: '#ffffff',
-    //             },
-    //             links: {
-    //                 color: '#ffffff',
-    //                 distance: 150,
-    //                 enable: true,
-    //                 opacity: 0.5,
-    //                 width: 1,
-    //             },
-    //             collisions: {
-    //                 enable: true,
-    //             },
-    //             move: {
-    //                 directions: 'none',
-    //                 enable: true,
-    //                 outModes: {
-    //                     default: 'bounce',
-    //                 },
-    //                 random: false,
-    //                 speed: 2,
-    //                 straight: false,
-    //             },
-    //             number: {
-    //                 density: {
-    //                     enable: true,
-    //                     area: 800,
-    //                 },
-    //                 value: 100,
-    //             },
-    //             opacity: {
-    //                 value: 0.5,
-    //             },
-    //             shape: {
-    //                 type: 'circle',
-    //             },
-    //             size: {
-    //                 value: { min: 1, max: 3 },
-    //             },
-    //         },
-    //         detectRetina: true,
-    //     };
-    // }, []);
     const particlesInit = useCallback(async (engine) => {
         console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
         await loadFull(engine);
     }, []);
 
